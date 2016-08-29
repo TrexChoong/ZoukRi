a860f0145f17e1c92e034af9a2a1dac23082f610
Trex <guanstrife@gmail.com>
2016-08-29 10:29:58 +0800

    Initial commit

---
+++ desktop/src/com/trexworkshop/www/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.trexworkshop.www.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.trexworkshop.www.ZoukRI;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZoukRI(), config);
+	}
+}
